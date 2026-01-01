2d9cc54d6a05747c0685490dfa6e0d19afb3c87e
Alina <alinavalentinaiacob@gmail.com>
2026-01-01 18:39:12 +0200

    add routes, main route

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
 from routes.main import router
+import uvicorn
 
 app = FastAPI(title="Structural Change API")
 
 app.include_router(router, prefix="/api")
+
+if __name__=="main":
+    uvicorn.run(
+        "main:app",
+        host="127.0.0.1",
+        port=8000,
+        reload=True)
+
